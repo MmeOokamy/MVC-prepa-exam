34a1feb08ba7b5bd9be1d47678701e041bd6d004
Manue <manue.ggt@gmail.com>
2020-12-07 12:12:26 +0100

    route & controller

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+/**
+ * recupéré la valeur de la super global get qui recupéré la variable page
+ */
+$page = filter_input(INPUT_GET, "page");
+
+/**
+ * Toutes les routes possibles avec leur controller associer
+ */
+$route = array(
+    "home" => HomeController::class,
+    "login" => LoginController::class,
+    "ads" => AllAdsController::class,
+    "ad" => AdController::class,
+    "newAD" => NewAdController::class
+);
